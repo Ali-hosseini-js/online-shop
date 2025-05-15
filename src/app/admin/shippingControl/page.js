451af12ddf77db1261a550284ceeb066f05cd968
Ali Hosseini <a75hosseini.js@gmail.com>
2025-05-15 08:45:57 -0700

    shipping

--- src/app/admin/shippingControl/page.js
+++ src/app/admin/shippingControl/page.js
@@ -1,7 +1,24 @@
-import React from "react";
+import ProductFrom from "@/module/product/ProductForm";
+import ProductList from "@/module/product/ProductList";
+import { Toaster } from "react-hot-toast";
 
-function ShippingControl() {
-  return <div>ShippingControl</div>;
+function ShippingControll() {
+  // if (!data) redirect("/");
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-col m-5 justify-center gap-5">
+        روش ارسال{" "}
+        <p className="text-main text-xl border-b-[1px] border-opinion mb-5"></p>
+        {/* <ProductFrom /> */}
+        <p className="text-main text-xl border-b-[1px] border-opinion mb-5">
+          لیست روش های ارسال{" "}
+        </p>
+        {/* <ProductList />{" "} */}
+      </div>
+      <Toaster />
+    </div>
+  );
 }
 
-export default ShippingControl;
+export default ShippingControll;
